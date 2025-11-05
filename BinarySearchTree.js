5229598a9f6163ea430899d2762fbc7b976ab57c
sunlightsky112 <sunlightsky4848@gmail.com>
2025-11-05 14:27:13 -0500

    fix: File lacks of an end of line

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -1,88 +1,87 @@
-
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 class BinarySearchTree {
-    constructor() {
-        this.root = null;
+  constructor() {
+    this.root = null;
+  }
+
+  // Insert a value into the BST (ignore duplicates)
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return;
     }
 
-    // Insert a value into the BST (ignore duplicates)
-    insert(value) {
-        const newNode = new Node(value);
-        if (!this.root) {
-            this.root = newNode;
-            return;
-        }
+    let current = this.root;
+    while (true) {
+      if (value === current.value) {
+        return; // Ignore duplicates
+      }
 
-        let current = this.root;
-        while (true) {
-            if (value === current.value) {
-                return; // Ignore duplicates
-            }
-
-            if (value < current.value) {
-                if (!current.left) {
-                    current.left = newNode;
-                    return;
-                }
-                current = current.left;
-            } else {
-                if (!current.right) {
-                    current.right = newNode;
-                    return;
-                }
-                current = current.right;
-            }
+      if (value < current.value) {
+        if (!current.left) {
+          current.left = newNode;
+          return;
         }
+        current = current.left;
+      } else {
+        if (!current.right) {
+          current.right = newNode;
+          return;
+        }
+        current = current.right;
+      }
     }
+  }
 
-    // Search for a value in the BST
-    search(value) {
-        let current = this.root;
-        while (current) {
-            if (value === current.value) {
-                return true;
-            }
-            current = value < current.value ? current.left : current.right;
-        }
-        return false;
+  // Search for a value in the BST
+  search(value) {
+    let current = this.root;
+    while (current) {
+      if (value === current.value) {
+        return true;
+      }
+      current = value < current.value ? current.left : current.right;
     }
+    return false;
+  }
 
-    // Inorder traversal
-    inorder(node = this.root, result = []) {
-        if (node) {
-            this.inorder(node.left, result);
-            result.push(node.value);
-            this.inorder(node.right, result);
-        }
-        return result;
+  // Inorder traversal
+  inorder(node = this.root, result = []) {
+    if (node) {
+      this.inorder(node.left, result);
+      result.push(node.value);
+      this.inorder(node.right, result);
     }
+    return result;
+  }
 
-    // Preorder traversal
-    preorder(node = this.root, result = []) {
-        if (node) {
-            result.push(node.value);
-            this.preorder(node.left, result);
-            this.preorder(node.right, result);
-        }
-        return result;
+  // Preorder traversal
+  preorder(node = this.root, result = []) {
+    if (node) {
+      result.push(node.value);
+      this.preorder(node.left, result);
+      this.preorder(node.right, result);
     }
+    return result;
+  }
 
-    // Postorder traversal
-    postorder(node = this.root, result = []) {
-        if (node) {
-            this.postorder(node.left, result);
-            this.postorder(node.right, result);
-            result.push(node.value);
-        }
-        return result;
+  // Postorder traversal
+  postorder(node = this.root, result = []) {
+    if (node) {
+      this.postorder(node.left, result);
+      this.postorder(node.right, result);
+      result.push(node.value);
     }
+    return result;
+  }
 }
 
 module.exports = BinarySearchTree;
